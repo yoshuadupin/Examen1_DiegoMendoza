e36e91b2df7e7a9eb69216398728eb81b454dff1
yoshuadupin <djosue@unitec.edu>
2017-02-17 14:28:50 -0600

    Clase Starks.h

---
+++ Starks.h
@@ -0,0 +1,40 @@
+//Clase Starks
+#include<string>
+#include<vector>
+
+class Starks{
+	private:
+
+		string jefeFamilia;
+		int cantLobosHuargo;
+		string animalEmblema;
+		string lema;
+		vector<PequenasFamilias*> ejercitoFamilias;
+		string guerreroValioso;
+		int cantIntegrantes;
+	public:
+
+		//Setter
+		void setJefeFamilia(string);
+		void setCantLobosHuargo(int);
+		void setAnimalEmblema(string);
+		void setLema(string);
+		//Metodos set del Vector
+		void setIntegranteFamilia(PequenasFamilias*);
+		
+		void setGuerreroValioso(string);
+		void setCantIntegrantes(int);
+		
+		//Getter
+		string getJefeFamilia();
+		int getCantLobosHuargo();
+		string getAnimalEmblema();
+		string getLema();
+		
+		//Metodos get del vector
+		PequenasFamilias* getIntegranteFamilia(int);
+		
+		string getGuerreroValioso();
+		int getCantIntegrantes();
+
+};
