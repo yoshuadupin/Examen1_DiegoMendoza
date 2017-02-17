2ac31a8d3175bb3991fe0c6a78a8dd942d6b9815
yoshuadupin <djosue@unitec.edu>
2017-02-17 16:46:19 -0600

    Another class marx will be shame to us

---
+++ GuardiaReal.cpp
@@ -0,0 +1,56 @@
+//Implementacion de las clases
+#include "GuardiaReal.h"
+//Setters
+void GuardiaReal::setNombre(string nombre){
+	this->nombre = nombre;
+}
+
+void GuardiaReal::setEdad(int edad){
+	this->edad = edad;
+}
+void GuardiaReal::setTipoSoldado(string tipoSoldado){
+	this->tipoSoldado = tipoSoldado;
+}
+void GuardiaReal::setAtaque(int ataque){
+	this->ataque = ataque;
+}
+void GuardiaReal::setDefensa(int defensa){
+	this->defensa = defensa;
+}
+//Getters
+string GuardiaReal::setNombre(){
+   return nombre;
+}
+
+int GuardiaReal::setEdad(){
+   return edad;
+}
+string GuardiaReal::setTipoSoldado(){
+  	return tipoSoldado;
+}
+int GuardiaReal::setAtaque(){
+  return ataque;
+}
+int GuardiaReal::setDefensa(){
+  return defensa;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
