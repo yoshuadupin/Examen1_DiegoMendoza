64d3e42757ed7ea88af42a926f0ca8cde8051c4a
yoshuadupin <djosue@unitec.edu>
2017-02-17 16:38:33 -0600

    GuardiaReal.h la desesperacion total abandonar toda esperanza

---
+++ GuardiaReal.h
@@ -0,0 +1,25 @@
+//Guardia real header
+#include<string>
+
+class GuardiaReal{
+	private:
+		string nombre;
+		int edad;
+		string tipoSoldado;
+		int ataque;
+		int defensa;
+	public:
+		//Setters
+		void setNombre(string);
+		void setEdad(int);
+		void setTipoSoldado(string);
+		void setAtaque(int);
+		void setDefensa(int);
+		//Getters
+		string getNombre(string);
+      int getEdad(int);
+      string getTipoSoldado(string);
+      int getAtaque(int);
+      int getDefensa(int);
+		//TODO:Constructores
+};
